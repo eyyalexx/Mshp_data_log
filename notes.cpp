5c07af7c07b90542d6500ec2708ae83ba1f465c9
eyyalexx <alex.grigorev@hotmail.com>
2015-11-21 15:50:08 -0800

    changed something

--- notes.cpp
+++ notes.cpp
@@ -9,11 +9,11 @@ const float x = 14.7;    //can't be changed. (const)
 const int y = 6;		//can't be changed.	(const)
 int num;
 
-
+void fred(int a, int *b);	//this is the prototype for the function made below.
 
 //"num_ptr1" and "num_ptr2" are pointers
 const int * num_ptr1;	//Preferred decleration
-int const * num_ptr2;	//Also compiles OKAY ^^
+int const * num_ptr2;	//Also compiles OKAY ^
 
 
 struct student{
@@ -50,6 +50,16 @@ int main (){	//"int" is the return type.
 
 	cout << stdnt1.fee << endl;
 
+
+	int x = 7, y = 8;
+
+	cout << "M1 " << x << " " << y << endl;
+
+	fred(x, &y);
+
+	cout << "M2 " << x << " " << y << endl;
+
+
 	return 0;
 	
 	//strcat(a, b); joins two things together ie. 2 characters
@@ -57,6 +67,16 @@ int main (){	//"int" is the return type.
 }
 
 
+void fred (int a, int * b){
+	cout << "F1 " << a << " " << *b << endl;
+
+	a = 3;
+	*b = 4;
+
+	cout << "F2 " << a << " " << *b << endl;
+
+}
+		
 
 
 
